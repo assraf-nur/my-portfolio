dd1cb992de6fdd0f29544e5306b0b339c37dd679
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-06-06 23:56:21 +0600

    polish projects

--- src/Components/Home/Projects.js
+++ src/Components/Home/Projects.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import total from '../../Images/total.png'
 import dvalley from '../../Images/dvalley.png'
-import doctors from '../../Images/doctor.png'
+import clicks from '../../Images/clicks.png'
+
 
 const Projects = () => {
     return (
-        <div className='mb-12'>
+        <div className='mb-12 px-12'>
             <h1 className='text-white text-center text-5xl mt-5'>My Latest Projects</h1>
-            <div class="card lg:card-side bg-base-100 shadow-xl mt-12 text-white">
+            <div class="card lg:card-side bg-base-100 shadow-xl mt-12 text-white w-8/12 mx-auto">
             <figure><img style={{width: '200px'}} src={total} alt="Album"/></figure>
             <div class="card-body">
                 <h2 class="card-title text-5xl">Total Tools</h2>
@@ -34,7 +35,7 @@ const Projects = () => {
             </div>
 
 
-            <div class="card lg:card-side bg-base-100 shadow-xl mt-12 text-white">
+            <div class="card lg:card-side bg-base-100 shadow-xl mt-12 text-white w-8/12 mx-auto">
             <figure><img style={{width: '200px'}} src={dvalley} alt="Album"/></figure>
             <div class="card-body">
                 <h2 class="card-title text-5xl">D-Valley</h2>
@@ -53,6 +54,27 @@ const Projects = () => {
                       stockout product. <br/>
                     • Admin can add and delete tool and can see
                     the user’s wish list.
+                </div>
+                </div>
+                <div class="card-actions justify-end">
+                </div>
+            </div>
+            </div>
+
+
+            <div class="card lg:card-side bg-base-100 shadow-xl mt-12 text-white w-8/12 mx-auto">
+            <figure><img style={{width: '200px'}} src={clicks} alt="Album"/></figure>
+            <div class="card-body">
+                <h2 class="card-title text-5xl">Mohsin-Clicks</h2>
+                <div>
+                <p className='text-white'><a target='_blank' className='text-xl ml-2 btn btn-sm text-white' href="https://mohsin-click.web.app/">Mohsin-Clicks Live</a></p>
+                <p className='mt-3 text-white'><a target='_blank' className='text-xl ml-2 btn btn-sm text-white' href="https://github.com/assraf-nur/Mohsin-clicks">Client Side Link</a></p>
+                <div className='mt-5 ml-2'>
+                    <p>Features</p>
+                    • User can choose the package <br/>
+                    • Can see the customers’ review <br/>
+                    • Various package and their details for user<br/>
+                    • Firebase Authentication for secure checkout.<br/>
                 </div>
                 </div>
                 <div class="card-actions justify-end">
