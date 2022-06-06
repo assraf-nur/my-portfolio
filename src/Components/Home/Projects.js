4536f0853743201982e9da4f6b3a2499b19772d3
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-06-06 23:49:11 +0600

    add projects

--- src/Components/Home/Projects.js
+++ src/Components/Home/Projects.js
@@ -5,12 +5,59 @@ import doctors from '../../Images/doctor.png'
 
 const Projects = () => {
     return (
-        <div>
+        <div className='mb-12'>
             <h1 className='text-white text-center text-5xl mt-5'>My Latest Projects</h1>
-            <div className='grid grid-cols-3 ml-42 gap-12 place-content-center mt-16'>
-                <img className='w-96' src={total} alt=""/>
-                <img className='w-96' src={dvalley} alt=""/>
-                <img className='w-96' src={doctors} alt=""/>
+            <div class="card lg:card-side bg-base-100 shadow-xl mt-12 text-white">
+            <figure><img style={{width: '200px'}} src={total} alt="Album"/></figure>
+            <div class="card-body">
+                <h2 class="card-title text-5xl">Total Tools</h2>
+                <div>
+                <p className='text-white'><a target='_blank' className='text-xl ml-2 btn btn-sm text-white' href="https://total-tools-47ca6.web.app/">Total-Tools Live</a></p>
+                <p className='mt-3 text-white'><a target='_blank' className='text-xl ml-2 btn btn-sm text-white' href="https://github.com/assraf-nur/Total-tools-client">Client Side Link</a></p>
+                <p className='mt-3 text-white'><a target='_blank' className='text-xl ml-2 btn btn-sm text-white' href="https://github.com/assraf-nur/Total-tools-server">Server Side Link</a></p>
+                <div className='mt-5 ml-2'>
+                    <p>Features</p>
+                    • User can order their desired tools <br/>
+                    • Firebase authentication activated for
+                    secure login and register. <br/>
+                    • Use has the payment option, if the user
+                    not paid yet he can cancel the order. <br/>
+                    • Admin can access the all-major options for
+                    supporting user. <br/>
+                    • Admin can add and delete tool and can see
+                    the user’s wish list.
+                </div>
+                </div>
+                <div class="card-actions justify-end">
+                </div>
+            </div>
+            </div>
+
+
+            <div class="card lg:card-side bg-base-100 shadow-xl mt-12 text-white">
+            <figure><img style={{width: '200px'}} src={dvalley} alt="Album"/></figure>
+            <div class="card-body">
+                <h2 class="card-title text-5xl">D-Valley</h2>
+                <div>
+                <p className='text-white'><a target='_blank' className='text-xl ml-2 btn btn-sm text-white' href="https://d-valley-2a581.web.app/">D-Valley Live</a></p>
+                <p className='mt-3 text-white'><a target='_blank' className='text-xl ml-2 btn btn-sm text-white' href="https://github.com/assraf-nur/D-valley-Client">Client Side Link</a></p>
+                <p className='mt-3 text-white'><a target='_blank' className='text-xl ml-2 btn btn-sm text-white' href="https://github.com/assraf-nur/D-valley-Server">Server Side Link</a></p>
+                <div className='mt-5 ml-2'>
+                    <p>Features</p>
+                    • User can manage products warehouse <br/>
+                    • Firebase authentication activated for
+                      secure login and register. <br/>
+                    • User can increase and decrease product
+                      quantity in manage inventory page.<br/>
+                    • User can add new products and delete
+                      stockout product. <br/>
+                    • Admin can add and delete tool and can see
+                    the user’s wish list.
+                </div>
+                </div>
+                <div class="card-actions justify-end">
+                </div>
+            </div>
             </div>
         </div>
     );
